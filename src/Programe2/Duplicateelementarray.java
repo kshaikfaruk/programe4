34315f3cd082fda0026f50dd7b2e1934ae456348
shaikfaruk <shaikfaruk04@gmail.com>
2022-05-23 19:02:25 +0530

    fifthpush

--- src/Programe2/Duplicateelementarray.java
+++ src/Programe2/Duplicateelementarray.java
@@ -3,15 +3,15 @@ package Programe2;
 public class Duplicateelementarray {
 
 	public static void main(String[] args) {
-	   int a[]={1,2,2,3,4,5,5,67};
-	   for(int i=0;i<=a.length-1;i++){
-		   for(int j=i+1;j<=a.length-1;j++){
-			   if(a[i]==a[j]){
-				   System.out.println("Duplicateelement"+a[i]);
-				   
-			   }
-		   }
-	   }
+		int a[]={1,2,3,4,4,5,5,6};
+		for(int i=0;i<=a.length-1;i++){
+			for(int j=i+1;j<=a.length-1;j++){
+				if(a[i]==a[j]){
+					System.out.println("Duplicateelement"+a[i]);
+					
+				}
+			}
+		}
 
 	}
 
--- src/Programe2/Findtwomax.java
+++ src/Programe2/Findtwomax.java
@@ -4,21 +4,25 @@ public class Findtwomax {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    int a[]={10,20,40,50,60};
-    int maxone=0;
-    int maxtwo=0;
-    for(int n:a){
-    	if(maxone<n){
-    		maxone=maxtwo;
-    		maxone=n;
-    	}
-    	if(maxtwo<n){
-    		maxtwo=n;
-    		
-    	}
-    	}
-    System.out.println(maxone);
-    System.out.println(maxtwo);
-    }
+       int a[]={10,20,40,50,60};
+       int maxone=0;
+       int maxtwo=0;
+       for(int n:a){
+    	   if(maxone<n){
+    		   maxone=maxtwo;
+    		   maxone=n;
+    		   
+    	   }else
+    		   if(maxtwo<n){
+    			   maxtwo=n;
+    			   
+    		   }
+    	   }
+    	   System.out.println(maxone);
+    	   System.out.println(maxtwo);
+    	   
+    	   
+       }
+	} 
 
-}
+
